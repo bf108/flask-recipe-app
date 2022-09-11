643dcc097cc353aa080e285402106a5c1d85f85a
Ben Farrell <ben.farrell@iqvia.com>
2022-09-11 20:49:09 +0100

    refactor __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -2,20 +2,27 @@ from flask import Flask, render_template, request, session, flash
 from pydantic import BaseModel, validator, ValidationError
 import logging
 
-#Logging Configuration
-file_handler = logging.FileHandler('flask_recipe_app.log')
-file_formatter = logging.Formatter(
-    '%(asctime)s %(levelname)s: %(message)s [in %(filename)s:%(lineno)d]')  # NEW!
-file_handler.setFormatter(file_formatter)
-file_handler.setLevel(logging.INFO)
+################################
+# Helper Functions
+################################
+
+def configure_logging(app):
+    file_handler = logging.FileHandler('flask_recipe_app.log')
+    file_formatter = logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(filename)s:%(lineno)d]')  # NEW!
+    file_handler.setFormatter(file_formatter)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('Recipe App is Starting...')
+
+def register_blueprints(app):
+    from src.ingredients import ingredients_blueprint
+    app.register_blueprint(ingredients_blueprint)
 
 def create_app():
     app = Flask(__name__, template_folder='templates')
     app.config.from_object('config.DevConfig')
-    app.logger.addHandler(file_handler)
-    app.logger.info('Recipe App is Starting...')
-
-    from src.ingredients import ingredients_blueprint
-    app.register_blueprint(ingredients_blueprint)
+    configure_logging(app)
+    register_blueprints(app)
 
     return app
