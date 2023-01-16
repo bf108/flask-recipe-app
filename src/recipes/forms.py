dee3ef55e64a6b7aa4f21a09815348a8249c540d
Ben Farrell <ben.farrell@iqvia.com>
2023-01-16 11:15:55 +0000

    basic form for bbc url - not functional

--- src/recipes/forms.py
+++ src/recipes/forms.py
@@ -1,9 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, SubmitField, SelectField, FloatField
-from wtforms.validators import DataRequired, Length, Optional
+from wtforms.validators import DataRequired, Length, Optional, ValidationError
 from src.models import Ingredient, Category
 from src import database as db
-from src.unit_converter.unit_converter import Units
+from src.unit_converter.unit_converter import UnitType
+from urllib.parse import urlparse
+import re
 
 def ingredient_choices():
     # return [(ing.id, ing.name) for ing in db.session.query(Ingredient).all()]
@@ -24,4 +26,19 @@ class IngredientRecipeForm(FlaskForm):
     category_id = SelectField('Category', validators=[DataRequired()], choices=category_choices)
     ingredient_id = SelectField('Ingredient', validators=[DataRequired()], choices=ingredient_choices)
     quantity = FloatField('Qty',validators=[DataRequired()])
-    unit = SelectField('Units', validators=[DataRequired()], choices=Units.ALL_UNITS)
+    unit = SelectField('Units', validators=[DataRequired()], choices=UnitType.ALL_UNITS)
+
+class BBCUrlForm(FlaskForm):
+    url = StringField('url', validators=[DataRequired()])
+
+    def validate_url(form, field):
+        "Custom Validator for URL"
+        res = urlparse(field.data)
+        scheme, netloc = res.scheme, res.netloc
+        if scheme != 'https':
+            raise ValidationError('URL must be https')
+        if netloc == "":
+            raise ValidationError('Invalid URL')
+        if netloc != 'www.bbc.co.uk':
+            raise ValidationError('URL must be from www.bbc.co.uk')
+
