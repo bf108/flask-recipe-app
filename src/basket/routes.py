1922102f642cd23e466ad738b914a5fb1d82abc0
Ben Farrell <ben.farrell@iqvia.com>
2023-01-21 10:07:06 +0000

    flag when recipe selected doesnt exist

--- src/basket/routes.py
+++ src/basket/routes.py
@@ -9,14 +9,14 @@ from sqlalchemy.exc import IntegrityError
 from . import baskets_blueprint
 from .forms import BasketForm
 import click
-from src.unit_converter.unit_converter import Units
+from src.unit_converter.unit_converter import UnitConverter
 
 ################################
 # Helper Function to list out Ingredients from Multiple Recipes
 ################################
 
 def aggregate_ingredients(basket):
-    uc = Units()
+    uc = UnitConverter()
     agg_ingredients_dict = {}
     for basket_recipe in basket.recipes:
         recipe_title = basket_recipe.recipes.title
@@ -25,7 +25,6 @@ def aggregate_ingredients(basket):
             item_name = ing.ingredients.name
             item_category = ing.ingredients.category.category
             item_id = ing.ingredient_id
-            #Account for recipe being selected multiple times
             item_qty = ing.quantity * multiplier
             item_unit = ing.unit
             item_metric = uc.metric_conversion(item_qty, item_unit)
@@ -42,6 +41,7 @@ def aggregate_ingredients(basket):
                 agg_ingredients_dict[item_name] = ingredient_quantities
 
     sorted_result = sorted([(k,v) for k, v in agg_ingredients_dict.items()], key=lambda x: (x[1]['category'], x[0]))
+    sorted_result = [(i,v) for i, v in enumerate(sorted_result)]
     return sorted_result
 
 ################################
@@ -53,20 +53,32 @@ def list_basket_recipes():
     current_basket =  Basket.query.order_by(Basket.id).first()
     agg_ing_dict = aggregate_ingredients(current_basket)
     if request.method == 'POST':
-        try:
-            tmp_item = BasketRecipes(
-                            basket_id=current_basket.id,
-                            recipe_id=form.recipe.data,
-                            quantity=form.quantity.data
-                            )
-            db.session.add(tmp_item)
-            db.session.commit()
-            flash(f'Added {tmp_item.recipes.title} to basket','success')
-            # current_app.logger.info(f'Create new recipe: {new_recipe.title}')
-            return redirect(url_for('baskets.list_basket_recipes'))
-        except IntegrityError:
-            db.session.rollback()
-            flash('Error adding recipe to basket','error')
+        rec_title = request.form['myRecipe']
+        if len(Recipe.query.filter_by(title=rec_title).all()) == 1:
+            try:
+                rec_id = Recipe.query.filter_by(title=rec_title).first().id
+                #If the recipe already exists, then replace value with new
+                existing_recipe_in_basket = BasketRecipes.query.filter_by(recipe_id=rec_id).first()
+                if existing_recipe_in_basket:
+                    existing_recipe_in_basket.quantity = form.quantity.data
+                    db.session.commit()
+                    flash(f'Updated {rec_title}','success')
+                else:
+                    tmp_item = BasketRecipes(
+                                    basket_id=current_basket.id,
+                                    recipe_id=rec_id,
+                                    quantity=form.quantity.data
+                                    )
+                    db.session.add(tmp_item)
+                    db.session.commit()
+                    flash(f'Added {tmp_item.recipes.title} to basket','success')
+                # current_app.logger.info(f'Create new recipe: {new_recipe.title}')
+                return redirect(url_for('baskets.list_basket_recipes'))
+            except IntegrityError:
+                db.session.rollback()
+                flash('Error adding recipe to basket','error')
+        else:
+            flash('Recipe not found - Please select available recipe','error')
     cbr = current_basket.recipes
     return render_template('basket_list.html',
                             cbr=cbr, agg_ing=agg_ing_dict, form=form)
